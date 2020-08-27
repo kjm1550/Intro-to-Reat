f48edc192ff80ac631fbdecba52f886a5ba18ec2
kjm1550 <kjm1550@gmail.com>
2020-08-26 21:36:06 -0500

    Updates

---
+++ precache-manifest.61bf097bcd3a252ef32ef16825a9def4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad05b8558cae22a2eb186713ff56972",
+    "url": "/Intro-to-Reat/index.html"
+  },
+  {
+    "revision": "b097a4f21278a625caf9",
+    "url": "/Intro-to-Reat/static/css/main.a0ad9318.chunk.css"
+  },
+  {
+    "revision": "3c8149f14d5b69d6c41e",
+    "url": "/Intro-to-Reat/static/js/2.12dc86f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Intro-to-Reat/static/js/2.12dc86f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b097a4f21278a625caf9",
+    "url": "/Intro-to-Reat/static/js/main.4c21c6a9.chunk.js"
+  },
+  {
+    "revision": "e1cccb8f7ff5b1925817",
+    "url": "/Intro-to-Reat/static/js/runtime-main.fd547611.js"
+  }
+]);
